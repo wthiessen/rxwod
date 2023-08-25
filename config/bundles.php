840aaaca0ae6d3d060ab8947672cc0a015f9cfbb
Will Thiessen <willthiessen@gmail.com>
2023-08-25 15:34:52 -0400

    Remove doctrine bundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,5 @@ return [
 //    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 //    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+//    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
