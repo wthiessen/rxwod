186c08fabec07e36a9891e75e1b28390cbb73261
Will Thiessen <willthiessen@gmail.com>
2023-08-27 16:57:09 -0400

    Update home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,10 +11,8 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="default")
      */
-    public function index(): JsonResponse
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'pong'
-        ]);
+        return $this->redirectToRoute('wod_list');
     }
 }
